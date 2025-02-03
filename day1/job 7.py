7c6898e43bfbdeff841fd5cd166f0bc6ce1916ec
Adeline <adeline.patenne@hotmail.fr>
2025-02-03 15:11:42 +0100

    fix: edit character class, update board inside class

--- day1/job 7.py
+++ day1/job 7.py
@@ -10,10 +10,11 @@ class Character():
 
     def move_up(self, board):
         self.clear_previous_emplacement(board)
-        if self.y > 0:
+        if self.y > 1:
             self.y -= 1
         else: 
             print("vous ne pouvez pas monter plus haut")
+        self.update_board(board)
     
     def move_down(self, board):
         self.clear_previous_emplacement(board)
@@ -21,13 +22,15 @@ class Character():
             self.y += 1
         else:
             print("vous ne pouvez pas descendre plus bas")
+        self.update_board(board)
     
     def move_left(self, board):
         self.clear_previous_emplacement(board)
-        if self.x > 0:
+        if self.x > 1:
             self.x -= 1
         else:
             print("vous ne pouvez pas aller sur la gauche")
+        self.update_board(board)
     
     def move_right(self, board):
         self.clear_previous_emplacement(board)
@@ -35,22 +38,49 @@ class Character():
             self.x += 1
         else:
             print("vous ne pouvez pas vous déplacer sur la droite")
+        self.update_board(board)
     
     def clear_previous_emplacement(self, board):
-        board[self.y][self.x] = ""
+        board[self.y][self.x] = " "
     
+    def update_board(self, board):
+        board[self.y][self.x] = self.sign
+        display_board(board)
+
     def position(self):
         return (self.x, self.y)
 
-little_one = Character(1, 0)
-board = [["", "", ""],["", "", ""],["", "", ""]]
+def display_board(board):
+    print("\n")
+    for i in range(len(board)):
+        for j in range(len(board)):
+            box = f"{board[i][j]} | "
+            print(box, end="")
+        print("")
+        line = "--+"
+        for index in range(len(board)-1):
+            line += "---+"
+        print(line)
+
+little_one = Character(2, 1)
+# board = [["", "", ""],["", "", ""],["", "", ""]]
+board =  [[" ", "A", "B", "C", "D"], [1," "," "," ", " "], [2," "," "," ", " "], [3," "," ", " ", " "], [4," "," ", " ", " "]]
 board[little_one.y][little_one.x] = little_one.sign
-print(board)
+display_board(board)
+
 little_one.move_down(board)
 little_one.move_down(board)
 little_one.move_right(board)
+# display_board(board)
+# little_one.update_board(board)
+
 little_one.move_left(board)
 little_one.move_left(board)
-board[little_one.y][little_one.x] = little_one.sign
-print(board)
-print(little_one.position())
+little_one.move_right(board)
+# little_one.update_board(board)
+
+little_one.move_right(board)
+little_one.move_up(board)
+
+# little_one.update_board(board)
+print(f"la position actuelle du personnage est : {little_one.position()}")
