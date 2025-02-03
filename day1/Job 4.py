fe84d75f021f4c75357110e16855fc7564d8e39d
Adeline <adeline.patenne@hotmail.fr>
2025-02-03 14:34:17 +0100

    fix: change present_yourself method in Person class

--- day1/Job 4.py
+++ day1/Job 4.py
@@ -3,16 +3,19 @@ print("\n=== JOB 4 ===")
 Job 4
 """
 class Person():
-    def __init__(self, firstname, lastname, age):
+    def __init__(self, firstname="", lastname="", age=""):
         self.lastname = lastname
         self.firstname = firstname
         self.age = age
 
     def present_yourself(self):
-        self.__str__()
+        self.lastname = input("Entrez votre nom : ")
+        self.firstname = input("Entrez votre prénom :")
+        self.age = input("entrez votre âge : ")
+        # self.__str__()
 
     def __str__(self):
-        print(f"Bonjour, je m'appelle {self.firstname} {self.lastname} et j'ai {self.age} ans.")
+        return f"Bonjour, je m'appelle {self.firstname} {self.lastname} et j'ai {self.age} ans."
 
 person1 = Person("Sari", "Norumai", 20)
 person2 = Person("Irahan", "Shuxi", 22)
@@ -20,8 +23,12 @@ person3 = Person("Malia", "Jodeli", 18)
 person4 = Person("Naku", "Atsona", 18)
 person5 = Person("Hashegin", "Ogin", 30)
 
-person1.present_yourself()
-person2.present_yourself()
-person3.present_yourself()
-person4.present_yourself()
-person5.present_yourself()
+person6 = Person()
+person6.present_yourself()
+
+print(person1)
+print(person2)
+print(person3)
+print(person4)
+print(person5)
+print(person6)
