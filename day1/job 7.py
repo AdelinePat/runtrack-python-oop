107bd2d10b819b8aa9280ae3d036a3762f11be45
Adeline <adeline.patenne@hotmail.fr>
2025-02-03 16:32:29 +0100

    feat: create_board method add to Board class, allow to define any board size

--- day1/job 7.py
+++ day1/job 7.py
@@ -1,3 +1,6 @@
+import string
+
+
 print("\n=== JOB 7 ===")
 """
 Job 7
@@ -12,75 +15,129 @@ class Character():
         self.clear_previous_emplacement(board)
         if self.y > 1:
             self.y -= 1
+            # self.update_board(board)
+            board.update_board(self)
         else: 
-            print("vous ne pouvez pas monter plus haut")
-        self.update_board(board)
+            print("\n vous ne pouvez pas monter plus haut")
+        
     
     def move_down(self, board):
         self.clear_previous_emplacement(board)
-        if self.y < len(board)-1:
+        if self.y < len(board.board)-1:
             self.y += 1
+            # self.update_board(board)
+            board.update_board(self)
         else:
-            print("vous ne pouvez pas descendre plus bas")
-        self.update_board(board)
+            print("\n vous ne pouvez pas descendre plus bas")
+        
     
     def move_left(self, board):
         self.clear_previous_emplacement(board)
         if self.x > 1:
             self.x -= 1
+            # self.update_board(board)
+            board.update_board(self)
         else:
-            print("vous ne pouvez pas aller sur la gauche")
-        self.update_board(board)
-    
+            print("\n vous ne pouvez pas aller sur la gauche")
+        
     def move_right(self, board):
         self.clear_previous_emplacement(board)
-        if self.x < len(board[0])-1:
+        if self.x < len(board.board[0])-1:
             self.x += 1
+            # self.update_board(board)
+            board.update_board(self)
         else:
-            print("vous ne pouvez pas vous déplacer sur la droite")
-        self.update_board(board)
+            print("\n vous ne pouvez pas vous déplacer sur la droite")
+        
     
     def clear_previous_emplacement(self, board):
-        board[self.y][self.x] = " "
+        board.board[self.y][self.x] = " "
     
-    def update_board(self, board):
-        board[self.y][self.x] = self.sign
-        display_board(board)
+    # def update_board(self, board):
+    #     board[self.y][self.x] = self.sign
+    #     board.display_board()
 
     def position(self):
         return (self.x, self.y)
+class Board():
+    # board =  [[" ", "A", "B", "C", "D"], [1," "," "," ", " "], [2," "," "," ", " "], [3," "," ", " ", " "], [4," "," ", " ", " "]]
+    alphabet = string.ascii_uppercase
 
-def display_board(board):
-    print("\n")
-    for i in range(len(board)):
-        for j in range(len(board)):
-            box = f"{board[i][j]} | "
-            print(box, end="")
-        print("")
-        line = "--+"
-        for index in range(len(board)-1):
-            line += "---+"
-        print(line)
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+        self.board = self.create_board()
 
-little_one = Character(2, 1)
-# board = [["", "", ""],["", "", ""],["", "", ""]]
-board =  [[" ", "A", "B", "C", "D"], [1," "," "," ", " "], [2," "," "," ", " "], [3," "," ", " ", " "], [4," "," ", " ", " "]]
-board[little_one.y][little_one.x] = little_one.sign
-display_board(board)
+    # def create_board(self):
+    #     first_row = [" ", "A", "B", "C", "D"]
+    #     board = []
+    #     row_list = []
+    #     for index in self.rows:
+    #         row_list = []
+    #         for other_index in self.columns:
+    #             if index == 0 and other_index == 0:
+    #                 content = " "
+    #                 row_list.append(content)
+    #             elif index == 0:
+    #                 content = self.alphabet[other_index]
+    #                 row_list.append(content)
+    #             else:
+    #                 content = " "
+    #                 row_list.append(content)
+
+    def create_board(self):
+        board = []
+        row_list = []
+        for index in range(self.rows):
+            row_list = []
+            for other_index in range(self.columns):
+                if index == 0 and other_index == 0:
+                    content = " "
+                    row_list.append(content)
+                elif index == 0:
+                    content = self.alphabet[other_index-1]
+                    row_list.append(content)
+                elif other_index == 0:
+                    content = index
+                    row_list.append(content)
+                else:
+                    content = " "
+                    row_list.append(content)
+            board.append(row_list)
+        return board
+
+
+    def display_board(self):
+        print("\n")
+        for i in range(len(self.board)):
+            for j in range(len(self.board)):
+                box = f"{self.board[i][j]} | "
+                print(box, end="")
+            print("")
+            line = "--+"
+            for index in range(len(self.board)-1):
+                line += "---+"
+            print(line)
+
+    def update_board(self, character):
+        self.board[character.y][character.x] = character.sign
+        self.display_board()
+            
+board = Board(6,6)
+little_one = Character(3,2)
+board.board[little_one.y][little_one.x] = little_one.sign
+
+board.display_board()
 
 little_one.move_down(board)
 little_one.move_down(board)
 little_one.move_right(board)
-# display_board(board)
-# little_one.update_board(board)
 
 little_one.move_left(board)
 little_one.move_left(board)
 little_one.move_right(board)
-# little_one.update_board(board)
 
 little_one.move_right(board)
 little_one.move_up(board)
 
-# little_one.update_board(board)
 print(f"la position actuelle du personnage est : {little_one.position()}")
