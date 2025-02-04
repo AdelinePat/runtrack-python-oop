d42c6f5350c2fa515e5be85ffb6133930c999352
Adeline <adeline.patenne@hotmail.fr>
2025-02-04 11:26:07 +0100

    feat: third job : create rent and return method with a defaultprivate attribute available

--- day 2/Job 2 - 3.py
+++ day 2/Job 2 - 3.py
@@ -1,8 +1,9 @@
 class Book():
-    def __init__(self, title, author, pages_number):
+    def __init__(self, title, author, pages_number, available = True):
         self.__title = title
         self.__author = author
         self.__pages_number = pages_number
+        self.__available = available
     
     def set_title(self, new_title):
         self.__title = new_title
@@ -25,13 +26,31 @@ class Book():
     def get_pages_number(self):
         return self.__pages_number
     
-    def __str__(self):
-        return f"Titre : {self.__title}\
-            \nAuteur : {self.__author}\
-            \nPages : {self.__pages_number}"
+    def get_availability(self):
+        return self.__available
+    
+    def check_availability(self):
+        if self.__available:
+            return True
+        else:
+            return False
+        
+    def rent_book(self):
+        if self.check_availability():
+            self.__available = False
+            print(f"\nVous avez emprunté {self.__title} avec succès")
+        else:
+            print("\nVous ne pouvez pas emprunter ce livre, attendez qu'il soit rendu pour pouvoir l'emprunter")
+
+    def return_book(self):
+        if not self.check_availability():
+            self.__available = True
+            print(f"\nVous avez rendu {self.__title} avec succès")
+        else:
+            print("\nVous ne pouvez pas rendre un livre que vous n'avez pas emprunté")
+
 
 my_book = Book("1984", "Georges Orwell", 482)
-# print(my_book)
 
 title = my_book.get_title()
 author = my_book.get_author()
@@ -58,3 +77,9 @@ my_book.set_pages_number(256)
 print(f"Titre : {my_book.get_author()}\
 \nAuteur : {my_book.get_title()}\
 \nPages : {my_book.get_pages_number()}\n")
+
+my_book.rent_book()
+print(my_book.get_availability())
+
+my_book.return_book()
+print(my_book.get_availability())
