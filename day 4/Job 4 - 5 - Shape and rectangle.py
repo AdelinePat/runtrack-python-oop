66b081660a0c9a28147b27d5cbba1b4ffdcce82e
Adeline <adeline.patenne@hotmail.fr>
2025-02-06 16:26:47 +0100

    feat: job 5 , create a circle class with a get_area method overloading get_area method from Shape

--- day 4/Job 4 - 5 - Shape and rectangle.py
+++ day 4/Job 4 - 5 - Shape and rectangle.py
@@ -9,7 +9,16 @@ class Rectangle(Shape):
     
     def get_area(self):
         return self.length * self.width
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def get_area(self):
+        return self.radius ** 2 * 3.14
+    
     
 a_rectangle = Rectangle(5,6)
-
-print(a_rectangle.get_area())
+a_circle = Circle(8)
+print(f"RECTANGLE : {a_rectangle.get_area()}")
+print(f"CIRCLE : {a_circle.get_area()}")
