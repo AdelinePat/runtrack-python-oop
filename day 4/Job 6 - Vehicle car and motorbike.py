64b5be723033e788e3c269919208c64f5cba5a12
Adeline <adeline.patenne@hotmail.fr>
2025-02-06 16:54:10 +0100

    feat : add start method to car class and motorbike class

--- day 4/Job 6 - Vehicle car and motorbike.py
+++ day 4/Job 6 - Vehicle car and motorbike.py
@@ -5,12 +5,14 @@ class Vehicle():
         self.year = year
         self.price = price
     
+    def start(self):
+        print("Attention, je roule !")
+    
     def __str__(self):
         return f"Marque : {self.brand}\
             \nModèle : {self.model}\
             \nAnnée : {self.year}\
             \nPrix : {self.price} €"
-    
 
 class Car(Vehicle):
     def __init__(self, brand, model, year, price):
@@ -23,6 +25,9 @@ class Car(Vehicle):
     def set_door(self, new_door_number):
         self.car_door = new_door_number
 
+    def start(self):
+        print("La voiture démarre !")
+
 class Motorbike(Vehicle):
     def __init__(self,brand, model, year, price):
         super().__init__(brand, model, year, price)
@@ -31,6 +36,9 @@ class Motorbike(Vehicle):
     def __str__(self):
         return super().__str__() + f"\nRoues : {self.wheel}"
 
+    def start(self):
+        print("La moto roule très vite")
+
 
 car = Car("Toyota", "Yaris", 1999, 20000)
 moto = Motorbike("Yamaha", "R1M", 2024, 27999)
@@ -38,3 +46,6 @@ car.set_door(3)
 print(car)
 print("\n")
 print(moto)
+
+car.start()
+moto.start()
